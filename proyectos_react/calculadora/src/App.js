5e4d607ec72b5b6e3be3415353d67a347fc277d8
Miriam Monje <isobelmonje@gmail.com>
2023-08-29 10:51:12 +0200

    Creamos pantalla y aplicamos propiedades css

--- proyectos_react/calculadora/src/App.js
+++ proyectos_react/calculadora/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import SofiaEnterpriseLogo from './imagenes/sofiaenterprise-logo.png';
 import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla';
+
 function App() {
   return (
     <div className='App'>
@@ -12,6 +14,7 @@ function App() {
 
       </div>
       <div className='contenedor-calculadora'>
+        <Pantalla />
         <div className='fila'>
           <Boton>1</Boton>
           <Boton>2</Boton>
