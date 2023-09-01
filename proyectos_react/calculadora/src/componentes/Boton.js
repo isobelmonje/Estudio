9ec0ce6123b11901961c9acff225160d49330493
Miriam Monje <isobelmonje@gmail.com>
2023-09-01 16:07:01 +0200

    Damos funcionalidad al botón clear y usamos eventlistener

--- proyectos_react/calculadora/src/componentes/Boton.js
+++ proyectos_react/calculadora/src/componentes/Boton.js
@@ -1,16 +1,18 @@
 import React from "react";
 import '../hojas-de-estilo/Boton.css';
+
 function Boton(props) {
 
     const esOperador = valor => {
         return isNaN(valor) && (valor != '.') && (valor != '=');
     };
-    return(
+    return (
         <div
-        className={`boton-contenedor ${esOperador(props.children)? 'operador' : ''}`.trimEnd()}>
+            className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}
+            onClick={() => props.manejarClic(props.children)}>
            {props.children} 
         </div>
-    )
+    );
 
 }
 
